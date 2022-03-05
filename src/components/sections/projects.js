0409e0009c35435129fcefd729eb058de99d77a9
Fabio Jun <fabiojundev@gmail.com>
2022-03-05 00:27:29 -0300

    Fix more site urls

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -147,7 +147,7 @@ const Projects = ({ data }) => {
   return (
     <StyledContainer>
       <StyledTitle ref={revealTitle}>Other Projects</StyledTitle>
-      <StyledArchiveLink to="https://github.com/fabiojundev" ref={revealArchiveLink}>
+      <StyledArchiveLink to="https://github.com/fabiojundev/resume/" ref={revealArchiveLink}>
         Visit my github profile
       </StyledArchiveLink>
 
