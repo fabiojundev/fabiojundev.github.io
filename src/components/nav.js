25f4e88cebfa4099b4ea4a4821e2c1f02b430d6a
Fabio Jun <fabiojundev@gmail.com>
2022-03-07 14:42:23 -0300

    Fix resume relative path

--- src/components/nav.js
+++ src/components/nav.js
@@ -294,7 +294,7 @@ class Nav extends Component {
                 <CSSTransition classNames={fadeDownClass} timeout={timeout}>
                   <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
                     <StyledResumeButton
-                      href="/resume.pdf"
+                      href="/resume/resume.pdf"
                       target="_blank"
                       rel="nofollow noopener noreferrer">
                       Resume
