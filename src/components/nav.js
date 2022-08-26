5c7c58ece87d1d04fc5cbad1c99000dbd81a99b9
Fabio Jun <fabiojundev@gmail.com>
2022-08-26 20:37:58 -0300

    Update CV

--- src/components/nav.js
+++ src/components/nav.js
@@ -294,7 +294,7 @@ class Nav extends Component {
                 <CSSTransition classNames={fadeDownClass} timeout={timeout}>
                   <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
                     <StyledResumeButton
-                      href="/fabiojun-resume.pdf"
+                      href="/fabiojun-cv.pdf"
                       target="_blank"
                       rel="nofollow noopener noreferrer">
                       Resume
